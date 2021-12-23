c967409d7eac8a2817b34c0d98ec825e7e5eef40
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-12-23 16:31:56 +0200

    run containers with python

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM python:3.10-slim-buster
 
-RUN apt update
-RUN apt install curl
-RUN curl -L -y "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-RUN chmod +x /usr/local/bin/docker-compose
+RUN pip install -r --no-cache-dir requirements.txt
 
 COPY . .
 
--- src/configure.py
+++ src/configure.py
@@ -1,29 +1,71 @@
 import os
 import secrets
 
+import docker
+
+DOCKER_NETWORK_NAME = "core"
+
 
 def create_secret(n: int) -> str:
     return secrets.token_urlsafe(n)
 
+MARKETPLACE_URL = os.environ.get("MARKETPLACE_URL")
+SECRET_KEY = create_secret(20)
+SUPER_USER_SECRET = create_secret(20)
+GATEWAY_DB_PASSWORD = create_secret(20)
+DASHBOARD_DB_PASSWORD = create_secret(20)
 
-def create_secrets():
-    os.environ["SECRET_KEY"] = create_secret(20)
-    os.environ["SUPER_USER_SECRET"] = create_secret(20)
-    os.environ["GATEWAY_DB_PASSWORD"] = create_secret(20)
-    os.environ["DASHBOARD_DB_PASSWORD"] = create_secret(20)
-    print("SECRET_KEY", os.environ["SECRET_KEY"])
-    print("SUPER_USER_SECRET", os.environ["SUPER_USER_SECRET"])
-    print("SECRET_KEY", os.environ["GATEWAY_DB_PASSWORD"])
-    print("SECRET_KEY", os.environ["DASHBOARD_DB_PASSWORD"])
+print("SECRET_KEY",         SECRET_KEY)
+print("SUPER_USER_SECRET",  SUPER_USER_SECRET)
+print("GATEWAY_DB_PASSWORD",         GATEWAY_DB_PASSWORD)
+print("SECRET_KEY",         DASHBOARD_DB_PASSWORD)
 
 
-def run_docker_compose():
-    os.system("docker-compose up")
+def deploy_containers():
+    client = docker.from_env()
+
+    client.networks.create(DOCKER_NETWORK_NAME, "bridge")
+
+    # Run gateway
+    client.containers.run(
+        "connectapi_gateway:latest",
+        name="connectapi_gateway",
+        hostname="gateway",
+        ports={80: 1687},
+        environment={
+            "SECRET_KEY": SECRET_KEY,
+            "SUPER_USER_SECRET": SUPER_USER_SECRET,
+            "MONGO_URL": f"mongodb://gatewayname:{GATEWAY_DB_PASSWORD}@5.183.9.78:27017/gateway",
+            "REDIS_PORT": 6379,
+            "REDIS_HOST": "redis",
+            "ENV": "PRODUCTION",
+        },
+        volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock', 'mode': 'rw'}},
+        detach=True,
+        network="core",
+    )
+
+    # Run dashboard
+    client.containers.run(
+        "connectapi_dashboard:latest",
+        name="connectapi_dashboard",
+        hostname="dashboard",
+        ports={80: 9934},
+        environment={
+            "SECRET_KEY": SECRET_KEY,
+            "mongo_url": f"mongodb://dashusername:{DASHBOARD_DB_PASSWORD}@5.183.9.78:27017/Dashboard",
+            "marketplace_url": MARKETPLACE_URL,
+            "gateway_url": "http://gateway",
+            "port": 80,
+        },
+        volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock', 'mode': 'rw'}},
+        detach=True,
+        network="core",
+    )
 
 
 def main():
-    create_secrets()
-    run_docker_compose()
+    deploy_containers()
 
 
 if __name__ == "__main__":
