e2599aeb51aaed0020f7cf2e58bb278b36d51169
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-12-24 14:46:15 +0200

    print only services names

--- src/test.py
+++ src/test.py
@@ -4,10 +4,11 @@ GATEWAY_DB_PASSWORD = "gatewaypass"
 
 
 def test_gateway_services():
+    # print all the saved services names
     c = pymongo.MongoClient(f"mongodb://gatewayname:{GATEWAY_DB_PASSWORD}@5.183.9.78:27017/gateway")
     db = c.get_default_database()
     col = db.get_collection("services")
-    print('\n'.join(map(str, col.find({}))))
+    print('\n'.join(map(str, col.find({}, {"name": 1}))))
 
 
 def main():
