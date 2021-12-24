afa4670f37550a0d594acc702ed512875cabe615
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-12-24 14:26:59 +0200

    convert to str

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ def test_gateway_services():
     c = pymongo.MongoClient(f"mongodb://gatewayname:{GATEWAY_DB_PASSWORD}@5.183.9.78:27017/gateway")
     db = c.get_default_database()
     col = db.get_collection("services")
-    print('\n'.join(col.find({})))
+    print('\n'.join(map(str, col.find({}))))
 
 
 def main():
