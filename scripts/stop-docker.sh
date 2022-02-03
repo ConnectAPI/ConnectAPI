a18a3c08d3bdc066ee1c0587b4f47061d8d7541a
yoyo <hvuhsg6@gmail.com>
2022-02-03 22:57:38 +0200

    no pruning

--- scripts/stop-docker.sh
+++ scripts/stop-docker.sh
@@ -2,4 +2,5 @@ docker run -it --rm \
 	 -v /var/run/docker.sock:/var/run/docker.sock \
 	 -v /opt:/opt:rw \
 	 -e MARKETPLACE_URL='https://market.boxs.ml' \
+	 -e PRUNE="no" \
 	 connectapihub/connectapi uninstall
