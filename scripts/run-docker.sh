38981f8cf1dbf8e7343c7bef56f2a08c41940da0
yoyo <hvuhsg6@gmail.com>
2022-02-25 13:54:46 +0200

    update marketplace url

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,6 +1,6 @@
 docker run -it --rm \
 	 -v /var/run/docker.sock:/var/run/docker.sock \
 	 -v /opt:/opt:rw \
-	 -e MARKETPLACE_URL='https://market.boxs.ml' \
+	 -e MARKETPLACE_URL='http://10.100.102.10:3596' \
 	 --network host \
 	 connectapihub/connectapi install
